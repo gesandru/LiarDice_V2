ab897ed0290869ac245a162e130e78911d00dea9
gesandru <machinekill7@gmail.com>
2025-11-26 17:28:03 +0100

    servidor principal manda a servidores replicados

--- LiarDice/src/servidor/Servidor.java
+++ LiarDice/src/servidor/Servidor.java
@@ -3,21 +3,29 @@ package servidor;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.concurrent.Executor;
+import java.util.Arrays;
+import java.util.List;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 //Podría añadir que el servidor se divida en 3 servidores, cada uno con su propio pool
 //de threads, para manejar mejor cargas
 public class Servidor {
+	
+	// Una lista de servidores replicados 
+	private static final List<Integer> replicas = Arrays.asList(55556, 55557, 55558);
+
+	private static int contador = 0;
+
     public static void main(String[] args) throws ClassNotFoundException {
         ExecutorService pool = Executors.newCachedThreadPool();
         try(ServerSocket ss = new ServerSocket(55555)){
             while(true) {
                 try{
                     Socket s = ss.accept();
-                    ThreadServer ts = new ThreadServer(s);
+                    ThreadServer ts = new ThreadServer(s, replicas, contador);
                     pool.execute(ts);
+                    contador++;
 
                 }
                 catch(Exception e) {
--- LiarDice/src/servidor/ThreadServer.java
+++ LiarDice/src/servidor/ThreadServer.java
@@ -2,178 +2,61 @@ package servidor;
 
 import java.io.*;
 import java.net.Socket;
-import java.util.ArrayList;
-
-import objetos.Dado;
-import objetos.Jugador;
+import java.util.List;
 
 public class ThreadServer implements Runnable {
-    private Socket socket;
+    private Socket cliente;
+    List<Integer> replicas;
+    int contador;
 
-    public ThreadServer(Socket s) {
-        this.socket = s;
+    public ThreadServer(Socket s, List<Integer> r, int c) {
+        this.cliente = s;
+        this.replicas = r;
+        this.contador = c;
     }
 
-    //Aquí se debería manejar todo el juego
+    //Indicamos a qué servidor mandarlo, por ahora se va mandando secuencialmente, 
+    //posible mejorarlo con un algorítmo pero es suficiente.
+    @Override
     public void run() {
-        try (ObjectOutputStream ow = new ObjectOutputStream(socket.getOutputStream());
-             ObjectInputStream objr = new ObjectInputStream(socket.getInputStream())) {
-            ArrayList<Jugador> jugadores = (ArrayList<Jugador>) objr.readObject();
-            ArrayList<Dado> todosDados;
-            int dadosJugadorAnterior;
-            //j es el numero de jugadores con manos vacias, es decir, sin dados
-            int i, j, k, l;
-            int dado1 = 0, dado2 = 0, dado3 = 0, dado4 = 0, dado5 = 0, dado6 = 0, contador = 0;
-            boolean winner = false;
-            boolean ronda = false;
-            String s = "";
-            String[] splitter;
-            String apuesta = "0 d1";
-            Jugador jugadorAnterior = null;
-            while (!winner) {
-                i = 0;
-                j = 0;
-                ronda = false;
-                todosDados = new ArrayList<Dado>();
-                while (i < jugadores.size()) {
-                    jugadores.get(i).Nuevoturno();
-                    todosDados.addAll(jugadores.get(i).getMano().getDados());
-                    if (jugadores.get(i).getMano().Vacio()) {
-                        j++;
-                    }
-                    i++;
-                }
-                if (j == jugadores.size() - 1) {
-                    ronda = true;
-                }
-                ow.writeInt(todosDados.size());
-                ow.flush();
-                //RESET AQUI
-                ow.reset();
-                ow.writeObject(jugadores);
-                ow.flush();
+        Socket backend = null;
 
-                dadosJugadorAnterior = jugadores.get(jugadores.size() - 1).getDadosEnMano();
-                while (!ronda) {
-                    i = i % jugadores.size();
-                    if (!jugadores.get(i).getMano().Vacio()) {
-                        ow.writeInt(dadosJugadorAnterior);
-                        ow.flush();
-                        s = (String) objr.readObject();
+        try {
+            // vamos mandando a cada servidor un cliente
+            int port = replicas.get(contador % replicas.size());
+            System.out.println("Thread " + contador + " → replica port " + port);
 
-                        if (s.startsWith("M")) {
-                            splitter = apuesta.split("d");
-                            k = Character.getNumericValue(splitter[0].charAt(0));
-                            l = Character.getNumericValue(splitter[1].charAt(0));
-                            dado1 = 0;
-                            dado2 = 0;
-                            dado3 = 0;
-                            dado4 = 0;
-                            dado5 = 0;
-                            dado6 = 0;
-                            contador = 0;
-                            while (contador < todosDados.size()) {
-                                switch (todosDados.get(contador).getNumero()) {
-                                    case 1 -> dado1++;
-                                    case 2 -> dado2++;
-                                    case 3 -> dado3++;
-                                    case 4 -> dado4++;
-                                    case 5 -> dado5++;
-                                    case 6 -> dado6++;
-                                }
-                                contador++;
-                            }
-                            switch (l) {
-                                case 1:
-                                    if (k > dado1) {
-                                        jugadorAnterior.QuitarDado();
-                                    } else {
-                                        jugadores.get(i).QuitarDado();
-                                    }
-                                    break;
-                                case 2:
-                                    if (k > dado2) {
-                                        jugadorAnterior.QuitarDado();
-                                    } else {
-                                        jugadores.get(i).QuitarDado();
-                                    }
-                                    break;
-                                case 3:
-                                    if (k > dado3) {
-                                        jugadorAnterior.QuitarDado();
-                                    } else {
-                                        jugadores.get(i).QuitarDado();
-                                    }
-                                    break;
-                                case 4:
-                                    if (k > dado4) {
-                                        jugadorAnterior.QuitarDado();
-                                    } else {
-                                        jugadores.get(i).QuitarDado();
-                                    }
-                                    break;
-                                case 5:
-                                    if (k > dado5) {
-                                        jugadorAnterior.QuitarDado();
-                                    } else {
-                                        jugadores.get(i).QuitarDado();
-                                    }
-                                    break;
-                                case 6:
-                                    if (k > dado6) {
-                                        jugadorAnterior.QuitarDado();
-                                    } else {
-                                        jugadores.get(i).QuitarDado();
-                                    }
-                                    break;
-                            }
-                            ronda = true;
-                            ow.writeBoolean(true);
-                            ow.flush();
-                        } else {
-                            apuesta = s;
-                            jugadorAnterior = jugadores.get(i);
-                            dadosJugadorAnterior = jugadorAnterior.getDadosEnMano();
-                            ow.writeBoolean(false);
-                            ow.flush();
-                        }
-                    }
-                    i++;
-                }
-                //vuelve a comprobar aquí si se ha terminado la partida
-                i = 0;
-                j = 0;
-                while (i < jugadores.size()) {
-                    if (jugadores.get(i).getMano().Vacio()) {
-                        j++;
-                    }
-                    i++;
-                }
-                if (j == jugadores.size() - 1) {
-                    winner = true;
-                }
-                ow.writeBoolean(winner);
-                ow.flush();
-            }
-            String name = "Empty";
-            i = 0;
-            while (i < jugadores.size()) {
-                if (!jugadores.get(i).getMano().Vacio()) {
-                    name = jugadores.get(i).getName();
-                }
-                i++;
-            }
-            ow.writeObject(name);
-        } catch (IOException e) {
-            e.printStackTrace();
-        } catch (ClassNotFoundException e) {
+            backend = new Socket("localhost", port);
+
+            // Conectamos al cliente con uno de los servidores para el juego
+            Thread t1 = proxy(cliente.getInputStream(), backend.getOutputStream());
+            Thread t2 = proxy(backend.getInputStream(), cliente.getOutputStream());
+
+            // esperamos a que termine
+            t1.join();
+            t2.join();
+
+        } catch (Exception e) {
             e.printStackTrace();
         } finally {
-            try {
-                socket.close();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
+            try { cliente.close(); } catch (Exception ignored) {}
+            try { if (backend != null) backend.close(); } catch (Exception ignored) {}
         }
+    }
+
+    private Thread proxy(InputStream in, OutputStream out) {
+        Thread t = new Thread(() -> {
+            try {
+                byte[] buf = new byte[4096];
+                int len;
+                while ((len = in.read(buf)) != -1) {
+                    out.write(buf, 0, len);
+                    out.flush();
+                }
+            } catch (IOException ignored) {}
+        });
+
+        t.start();
+        return t;
     }
 }
